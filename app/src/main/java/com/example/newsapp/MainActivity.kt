6e7f5f2ff721572fb8208f328caae0275b58ed5f
rawani53 <work.pranav53@gmail.com>
2024-11-25 00:08:10 +0530

    coloum and text added

---
+++ app/src/main/java/com/example/newsapp/MainActivity.kt
@@ -0,0 +1,48 @@
+package com.example.newsapp
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.sp
+import com.example.newsapp.ui.theme.NewsappTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            NewsappTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Column(
+                        modifier = Modifier
+                            .padding(innerPadding)
+                            .fillMaxSize()
+                    ) {
+                        Text(
+                            text = "DOON 24",
+                            modifier = Modifier.align(Alignment.CenterHorizontally),
+                            color = Color.Red,
+                            fontSize = 25.sp,
+                            fontFamily = FontFamily.Serif
+                        )
+                    }
+
+                }
+            }
+        }
+    }
+}
+
+
