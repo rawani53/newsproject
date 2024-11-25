d7538585ae92f4cc52ff043c796ae34995ed85f3
rawani53 <work.pranav53@gmail.com>
2024-11-25 16:20:29 +0530

    outlined text feild for search bar added in expanded position of, trailing icon to be added

--- app/src/main/java/com/example/newsapp/Homepage.kt
+++ app/src/main/java/com/example/newsapp/Homepage.kt
@@ -1,12 +1,14 @@
 package com.example.newsapp
 
 import android.graphics.drawable.Icon
+import androidx.compose.foundation.border
 import androidx.compose.foundation.horizontalScroll
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
@@ -14,6 +16,7 @@ import androidx.compose.foundation.lazy.LazyItemScope
 import androidx.compose.foundation.lazy.LazyListScope
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Search
 import androidx.compose.material3.Button
@@ -21,6 +24,7 @@ import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
@@ -30,6 +34,8 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
@@ -98,9 +104,18 @@ fun CategoriesBar(viewModel: NewsviewModel) {
 
         if (isSearchExpanded) {
 
+            OutlinedTextField(
+
+                modifier = Modifier.padding(8.dp).height(48.dp).border(1.dp, Color.Gray, CircleShape)
+                    .clip(CircleShape),
+                value = searchQuery,
+                onValueChange = { searchQuery = it })
+
         } else {
+
             IconButton(onClick = {
                 isSearchExpanded = true
+
             }) {
 
                 Icon(
