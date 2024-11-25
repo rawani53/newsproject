ffd9a26c75885b2ff1e15eafe50abb02f1875a6e
rawani53 <work.pranav53@gmail.com>
2024-11-25 16:48:38 +0530

    trailing icon added and confiured to collapse search bar alongwith sending query to the new everything api to fetch searched news

--- app/src/main/java/com/example/newsapp/NewsviewModel.kt
+++ app/src/main/java/com/example/newsapp/NewsviewModel.kt
@@ -6,6 +6,7 @@ import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.kwabenaberko.newsapilib.NewsApiClient
 import com.kwabenaberko.newsapilib.models.Article
+import com.kwabenaberko.newsapilib.models.request.EverythingRequest
 import com.kwabenaberko.newsapilib.models.request.TopHeadlinesRequest
 import com.kwabenaberko.newsapilib.models.response.ArticleResponse
 
@@ -17,6 +18,29 @@ class NewsviewModel : ViewModel() {
 
     val _articles = MutableLiveData<List<Article>>()
     val articles: LiveData<List<Article>> = _articles
+
+    fun fetchEverythingWithQuery(query : String) {
+
+        val newsApiClient = NewsApiClient(constant.apikey)
+
+        val request = EverythingRequest.Builder().language("en").q(query).build()
+
+        newsApiClient.getEverything(request, object : NewsApiClient.ArticlesResponseCallback {
+            override fun onSuccess(response: ArticleResponse?) {
+                response?.articles?.let {
+                    _articles.postValue(it)
+                }
+            }
+
+            override fun onFailure(throwable: Throwable?) {
+                if (throwable != null) {
+                    Log.i("NEWSAPI Response failed", throwable.localizedMessage)
+                }
+
+            }
+
+        })
+    }
 
     fun fetchNewsTopHeadlines(category: String = "GENERAL") {
 
