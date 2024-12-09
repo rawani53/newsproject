64e991c29e5f8fe49442e5c01c43b6171333384b
rawani53 <work.pranav53@gmail.com>
2024-12-09 16:48:42 +0530

    login screen ui updated

--- app/src/main/java/com/example/newsapp/LoginScreen.kt
+++ app/src/main/java/com/example/newsapp/LoginScreen.kt
@@ -19,6 +19,7 @@ import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.OutlinedTextFieldDefaults
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
@@ -29,19 +30,22 @@ import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.input.PasswordVisualTransformation
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.lifecycle.ViewModel
 import androidx.navigation.NavController
 
 @Composable
-fun LoginScreen(navController: NavController) {
+fun LoginScreen(viewModel: NewsviewModel, navController: NavController) {
 
     var email by remember { mutableStateOf("") }
     var password by remember { mutableStateOf("") }
@@ -58,13 +62,16 @@ fun LoginScreen(navController: NavController) {
                 context = context,
                 scope = scope,
                 launcher = null,
-                logintoast = { Toast.makeText(context, "Login successful", Toast.LENGTH_SHORT).show() },
-                login = { navController.navigate(HomePageScreen)}
+                logintoast = {
+                    Toast.makeText(context, "Login successful", Toast.LENGTH_SHORT).show()
+                },
+                login = { navController.navigate(HomePageScreen) }
             )
 
 
         }
 
+    // UI
 
     Box {
 
@@ -76,50 +83,68 @@ fun LoginScreen(navController: NavController) {
             modifier = Modifier.fillMaxSize()
         )
 
+
         Box(
             modifier = Modifier
                 .matchParentSize()
                 .background(
-                    Brush.verticalGradient(
+                    Brush.linearGradient(
                         colors = listOf(
-                            Color.Transparent,
-                            MaterialTheme.colorScheme.tertiary
+                            Color.Magenta.copy(.3f),
+                            MaterialTheme.colorScheme.primary.copy(.3f),
+                            Color.Blue.copy(.6f)
                         ),
-                        startY = 600f
+                        start = Offset(0f, 0f),
+                        end = Offset(1200f, 1200f)
                     )
                 )
         )
+
+
         Column(
             modifier = Modifier.fillMaxSize(),
             verticalArrangement = Arrangement.Center,
             horizontalAlignment = Alignment.CenterHorizontally
         ) {
 
+
             Image(
-                painter = painterResource(id = R.drawable.icons8inshorts100),
+                painter = painterResource(id = R.drawable.loginsceenlogo),
                 contentDescription = "login image",
-                modifier = Modifier.size(200.dp)
+                modifier = Modifier.size(150.dp)
             )
 
-            Text("NewsNation", fontSize = 28.sp, fontWeight = FontWeight.Bold)
+            Text(
+                "NewsNation", fontSize = 28.sp, fontWeight = FontWeight.Bold, color = Color.Black
+            )
+
 
             Spacer(modifier = Modifier.height(16.dp))
 
-            Text("Login to your account")
+
+            Text("Login to your account", color = Color.DarkGray)
+
 
             OutlinedTextField(value = email, onValueChange = { email = it }, label = {
-                Text("Email address")
-            })
 
-            Spacer(modifier = Modifier.height(16.dp))
-
-            OutlinedTextField(value = password, onValueChange = { password = it }, label = {
-                Text("Password")
-            },
-                visualTransformation = PasswordVisualTransformation()
+                Text("Email address", color = Color.DarkGray)
+            }
             )
 
+
             Spacer(modifier = Modifier.height(16.dp))
+
+
+            OutlinedTextField(value = password, onValueChange = { password = it }, label = {
+                Text("Password", color = Color.DarkGray)
+            },
+                visualTransformation = PasswordVisualTransformation(),
+                colors = OutlinedTextFieldDefaults.colors(Color.DarkGray)
+            )
+
+
+            Spacer(modifier = Modifier.height(16.dp))
+
 
             Button(onClick = {
                 GoogleSignInUtils.doGoogleSignIn(
@@ -129,26 +154,36 @@ fun LoginScreen(navController: NavController) {
                     logintoast = {
                         Toast.makeText(context, "Login successful", Toast.LENGTH_LONG).show()
                     },
-                    login = { navController.navigate(HomePageScreen) },
+                    login = { navController.navigate(HomePageScreen) }
                 )
+                viewModel.fetchNewsTopHeadlines()
             }) {
+
                 Text("Login")
             }
 
+
             Spacer(modifier = Modifier.height(11.dp))
 
+
             TextButton(onClick = {}) {
-                Text("Forgot Password?")
+                Text(
+                    "Forgot Password?", color = Color.DarkGray
+                )
             }
 
+
             Spacer(modifier = Modifier.height(32.dp))
+
 
             Text("Or sign in with")
 
 
             Row(
 
-                modifier = Modifier.fillMaxWidth().padding(40.dp),
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(40.dp),
                 horizontalArrangement = Arrangement.SpaceEvenly
 
             ) {
@@ -156,31 +191,41 @@ fun LoginScreen(navController: NavController) {
 
                 Image(painter = painterResource(id = R.drawable.facebook),
                     contentDescription = "facebook",
-                    modifier = Modifier.size(60.dp).clickable {
-                        //click action
-                    })
+                    modifier = Modifier
+                        .size(60.dp)
+                        .clickable {
+                            //click action
+                        })
+
 
                 Image(painter = painterResource(id = R.drawable.google),
                     contentDescription = "google",
-                    modifier = Modifier.size(60.dp).clickable {
-                        //click action
-                        GoogleSignInUtils.doGoogleSignIn(
-                            context = context,
-                            scope = scope,
-                            launcher = launcher,
-                            logintoast = {
-                                Toast.makeText(context, "Login successful", Toast.LENGTH_SHORT)
-                                    .show()
-                            },
-                            login = { navController.navigate(HomePageScreen) },
-                        )
-                    })
+                    modifier = Modifier
+                        .size(60.dp)
+                        .clickable {
+                            //click action
+                            GoogleSignInUtils.doGoogleSignIn(
+                                context = context,
+                                scope = scope,
+                                launcher = launcher,
+                                logintoast = {
+                                    Toast
+                                        .makeText(context, "Login successful", Toast.LENGTH_SHORT)
+                                        .show()
+                                },
+                                login = { navController.navigate(HomePageScreen) }
+                            )
+                            viewModel.fetchNewsTopHeadlines()
+                        })
+
 
                 Image(painter = painterResource(id = R.drawable.twitter),
                     contentDescription = "twitter",
-                    modifier = Modifier.size(60.dp).clickable {
-                        //click action
-                    })
+                    modifier = Modifier
+                        .size(60.dp)
+                        .clickable {
+                            //click action
+                        })
 
             }
 
